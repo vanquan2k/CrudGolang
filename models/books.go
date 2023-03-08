2d0565cca211520e7e62037883b69ef6a040ca80
Dang Van Dung <jeremy.da@easysunday.com>
2023-03-08 15:54:55 +0700

    first commit

---
+++ models/books.go
@@ -0,0 +1,21 @@
+package models
+
+
+import "gorm.io/gorm"
+
+
+
+type Books struct {
+	ID            uint `gorm:"primary key ; autoIncrement " ison:"id"`
+	Author			*string    `json:"author"`
+	Title   *string   `json:"title"`
+	Publisher  *string  `json:"publisher"`
+}
+
+
+
+
+func MigrateBooks (db *gorm.DB) error {
+	err := db.AutoMigrate(&Books{})
+	return err 
+}
